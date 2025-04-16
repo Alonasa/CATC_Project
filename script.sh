2673f4736158b478c3eec247e3e5d7af510c6b4e
Alonasa <alona.s.kry@gmail.com>
2025-04-16 16:48:40 +0000

    created and check execution of  script for date and hostname commands

---
+++ script.sh
@@ -0,0 +1,27 @@
+
+#!/bin/bash
+# Author: Alona Skrypnyk
+# Date: 16/04/2025
+# Description: Script for grouping
+# and executing marked commands
+
+# Execution script result file
+OUTPUT_FILE="alonaSkrypnyk.txt"
+
+# Clear file if it's already exists
+>"$OUTPUT_FILE"
+
+#Script for execution command date
+#Append command name to the required file
+echo "Date: " >> "$OUTPUT_FILE"
+#Append result of date command to the file
+date >> "$OUTPUT_FILE"
+#Append empty line after command execution to make file look better
+echo "" >> "$OUTPUT_FILE"
+
+#Script for execution command hostname
+echo "Hostname: " >> "$OUTPUT_FILE"
+hostname >> "$OUTPUT_FILE"
+echo "" >> "$OUTPUT_FILE"
+
+
