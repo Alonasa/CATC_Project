a684a50a089293037d85e2c855e238a21e0e7518
Alonasa <alona.s.kry@gmail.com>
2025-04-19 20:15:28 +0000

    make a script work with expected input types and number arguments

--- scripts/salary-table-gen.sh
+++ scripts/salary-table-gen.sh
@@ -3,6 +3,7 @@
 #Author: Alona Skrypnyk
 #Description: Calculate salary progression to expected salary in years.
 #Based on that data generate table
+current_year=2025
 echo ==============================================
 echo "    Welcome to the salary scale generator   "
 echo ==============================================
@@ -11,7 +12,29 @@ read -p "Enter your starting salary and the number of points on the salary
 scale: " current_salary points
 read -p "Enter salary increment: " salary_increment 
 read -p "Are you manager? Input yes or no: " is_manager
+#checks if user input first character is yes for manager question
 
-resulting=$(( $points * ($current_salary + $salary_increment) ))
-echo "You will reach $resulting salary in $points years"
+shortened_manager=${is_manager:0:1}
+
+if [[ ${shortened_manager,,} == "y" ]]
+then
+	salary_increment=$(( salary_increment * 2 ))
+fi
+echo " ____________________________________________ "
+echo "|             |                              |"
+echo "|     Year    |         Expected salary      |"
+echo "|_____________|______________________________|"
+
+for ((n=1; n<=$points; n++))
+do
+	if [ "$n" -eq 1 ]
+	then
+		echo "|     $current_year    |              $(( current_salary + (salary_increment * n) ))           |"              
+		echo "|_____________|______________________________|"
+        else
+		echo "|     $(( current_year  + n ))    |              $((current_salary + (salary_increment * n)))           |"
+                echo "|_____________|______________________________|"
+	fi
+
+done
 
